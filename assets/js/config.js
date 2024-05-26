cbeb88f90e24c3ed906ec399ad86a49e78febccf
root-mega <65620837+root-mega@users.noreply.github.com>
2024-05-26 02:09:55 +0200

    First commit

---
+++ assets/js/config.js
@@ -0,0 +1,31 @@
+const BADGES = [
+  {
+    name: "XiaoPS", // Message that will appear when hovering over the badge
+    image: "assets/badges/verified_bot_developer.svg",
+  },
+  {
+    name: "Untouchable",
+    image: "assets/badges/server_owner.svg"
+  }
+];
+
+const USER_ID = "1237057255078101002"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
